4d399b710634d8216fc647cdfa1ad8877d1ec7db
Irina Pospelova <irapospelova78@gmail.com>
2024-01-23 19:47:16 +0300

    Initial commit

--- 7.3/jest/test/unit/sortByName.test.js
+++ 7.3/jest/test/unit/sortByName.test.js
@@ -14,4 +14,23 @@ describe("Books names test suit", () => {
       "Гарри Поттер",
     ]);
   });
+
+it("Books names should be sorted in descending order", () => {
+  const input = ["Властелин Колец", "Волшебник изумрудного города", "Гарри Поттер"];
+  const expected = ["Властелин Колец", "Волшебник изумрудного города", "Гарри Поттер"];
+  expect(sorting.sortByName(input)).toEqual(expected);
+});
+
+it("Might have an empty array", () => {
+  const input = [];
+  const expected = [];
+  expect(sorting.sortByName(input)).toEqual(expected);
+});
+
+it("Should return 0 when names are equal", () => {
+  const input = ["Гарри Поттер", "гарри поттер"];
+  const expected = input;
+  expect(sorting.sortByName(input)).toEqual(expected); 
+});
+
 });
